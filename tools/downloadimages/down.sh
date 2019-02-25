53587f2832f336cc53b533e39c69c583852bf003
AHaliq <liqo12@hotmail.com>
2019-02-25 13:24:16 +0800

    changed to donwload via wget

--- tools/downloadimages/down.sh
+++ tools/downloadimages/down.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#git clone https://github.com/chentinghao/download_google_drive.git
 
-mob=1eGHN4KcMl6X4SAAoFOe2m8SRZLXXkWLW
-bea=1M-UK5YmSYVEaClyWq3RNNkHctQ2qvQk9
-fas=1nVA1iZBUS79WQghO9DXg54qgxnw9OSI3
+mob=https://www.dropbox.com/s/ss2ibxu0k4x9c91/mobile_image.tar.gz?dl=1
+bea=https://www.dropbox.com/s/bhy4b7bwsvvhtf6/beauty_image.tar.gz?dl=1
+fas=https://www.dropbox.com/s/3jpwfbeilm22vhs/fashion_image.tar.gz?dl=1
 mobf=mobile_image.tar.gz
 beaf=beauty_image.tar.gz
 fasf=fashion_image.tar.gz
 dir=../tars/
 
-python download_gdrive.py "$mob" "$dir$mobf"
-#python download_gdrive.py "$bea" "$dir$beaf"
-#python download_gdrive.py "$fas" "$dir$fasf"
+#wget -O "$mobf" "$mob"
+wget -O "$beaf" "$bea"
+#wget -O "$fasf" "$fas"
